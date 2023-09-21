85436d43e3cd65525685d256cb7681d32454278d
Salavat Safin <safin.salavat45@mail.ru>
2023-09-21 09:55:19 +0300

    Project2

--- src/channel.py
+++ src/channel.py
@@ -11,9 +11,34 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
+        self.title = self.get_info()['items'][0]['snippet']['title']
+        self.description = self.get_info()['items'][0]['snippet']['description']
+        self.url = self.get_info()['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = self.get_info()['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.get_info()['items'][0]['statistics']['videoCount']
+        self.view_count = self.get_info()['items'][0]['statistics']['viewCount']
 
-    def print_info(self) -> None:
+    @property
+    def channel_id(self):
+        return self._channel_id
+
+    def get_info(self):
+        """Возвращает информацию о канале."""
+        channel = self.get_service().channels().list(id=self._channel_id, part='snippet,statistics').execute()
+        return channel
+
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.get_service().channels().list(id=self._channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return youtube
+
+    def to_json(self, name):
+        data = {'id': self.channel_id, 'title': self.title, 'description': self.description, 'url': self.url, 'subscriber count': self.subscriber_count, 'video count': self.video_count, 'view count': self.view_count}
+        with open(name, 'w') as file:
+            json.dump(data, file)
+
