cf5eb042a0f035f380b28af3f9d47d3987260e9b
Salavat Safin <safin.salavat45@mail.ru>
2023-10-02 21:19:24 +0500

    Project6

--- src/video.py
+++ src/video.py
@@ -8,16 +8,21 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Video:
     def __init__(self, id_video):
         self.id_video = id_video
-        self.name = self.video_response()['items'][0]['snippet']['title']
         self.url = f'https://youtu.be/{self.id_video}'
-        self.view_count = self.video_response()['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response()['items'][0]['statistics']['likeCount']
+        try:
+            self.title = self.video_response()['items'][0]['snippet']['title']
+            self.view_count = self.video_response()['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response()['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
 
     def video_response(self):
         return youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',id=self.id_video).execute()
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
 
 
 class PLVideo(Video):
--- tests/test_video.py
+++ tests/test_video.py
@@ -12,10 +12,21 @@ def video2():
     return PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
 
 
+@pytest.fixture
+def broken_video():
+    return Video('broken_video_id')
+
+
 def test_video1(video1):
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
 
 
 def test_video2(video2):
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
+
+
+def test(broken_video):
+    assert broken_video.id_video == 'broken_video_id'
+    assert broken_video.title is None
+    assert broken_video.like_count is None
 
